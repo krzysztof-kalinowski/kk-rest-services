9eac3a5cad614431a3cdb2105274552f3ba6ee4d
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-12-17 16:49:32 +0100

    first commit

---
+++ src/test/java/kalinowski/krzysztof/restservices/RestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package kalinowski.krzysztof.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
